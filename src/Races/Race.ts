7e6307fac2db4102bb76350f3ecacca3e4e538a4
Philip Lages <psilvaelages@gmail.com>
2023-01-18 15:30:22 -0300

    feat: creates abstract class Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,3 +1,25 @@
 export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+  private static _instanceCounter = 0;
 
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    Race._instanceCounter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    throw Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this.name;
+  }
+
+  get dexterity(): number {
+    return this.dexterity;
+  }
 }
